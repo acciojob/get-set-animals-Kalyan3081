c9afd56f9bf5fcbb89a58907e56b19984b771f75
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-10-18 17:38:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-//complete this code
-class Animal {}
+// Animal class
+class Animal {
+    constructor(species) {
+        this._species = species;
+    }
+    
+    // Getter for species
+    get species() {
+        return this._species;
+    }
 
-class Dog extends Animal {}
+    // Method to log the sound of the animal
+    makeSound() {
+        console.log(`The ${this._species} makes a sound`);
+    }
+}
 
-class Cat extends Animal {}
+// Dog class extending Animal
+class Dog extends Animal {
+    // Method to log bark sound
+    bark() {
+        console.log("woof");
+    }
+}
+
+// Cat class extending Animal
+class Cat extends Animal {
+    // Method to log purr sound
+    purr() {
+        console.log("purr");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
